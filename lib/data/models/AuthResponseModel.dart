d2067ecb2b95f538d4fdcab6d77f9ba5554e0223
Dev-AlexanderEP <dev.alexander.estrada@gmail.com>
2025-09-17 23:43:24 -0500

    Add: Se aniadio AuthResponseModel.dart este representa la respuesta del bakend, los tokens de acceso.

---
+++ lib/data/models/AuthResponseModel.dart
@@ -0,0 +1,23 @@
+class AuthResponseModel {
+  final String accessToken;
+  final String refreshToken;
+
+  AuthResponseModel({
+    required this.accessToken,
+    required this.refreshToken,
+  });
+
+  factory AuthResponseModel.fromJson(Map<String, dynamic> json) {
+    return AuthResponseModel(
+      accessToken: json['accessToken'],
+      refreshToken: json['refreshToken'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "accessToken": accessToken,
+      "refreshToken": refreshToken,
+    };
+  }
+}
