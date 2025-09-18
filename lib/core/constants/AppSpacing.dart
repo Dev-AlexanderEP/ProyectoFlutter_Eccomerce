bc81542c234eab76dd4b2699cee8c30d430291eb
Dev-AlexanderEP <dev.alexander.estrada@gmail.com>
2025-09-17 23:38:19 -0500

    Add: Se aniadio AppSpacing.dart para el espaciado repetitivo en la UI

---
+++ lib/core/constants/AppSpacing.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+class AppSpacing {
+  static const padCustom = EdgeInsets.symmetric(horizontal: 24, vertical: 10);
+  static const small = EdgeInsets.all(8);
+  static const medium = EdgeInsets.all(16);
+  static const large = EdgeInsets.all(24);
+}
