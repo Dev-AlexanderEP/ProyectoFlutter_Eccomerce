0ca044a00ce8e2ade6f4640b7417fbb0acb7d287
Dev-AlexanderEP <dev.alexander.estrada@gmail.com>
2025-09-27 17:18:14 -0500

    refactor: update ApiConstants.dart

--- lib/core/constants/ApiConstants.dart
+++ lib/core/constants/ApiConstants.dart
@@ -1,11 +1,15 @@
 class ApiConstants {
-  static const String baseUrl = "http://192.168.166.135:8080/api/v1";
-  static const String baseUrlL = "http://192.168.166.135:8080";
+  static const String baseUrl = "http://192.168.10.135:8080/api/v1";
+  static const String baseUrlL = "http://192.168.10.135:8080";
 
   // Login
   static const String loginEndpoint = "$baseUrlL/token";
-
   // Login Google
   static const String googleLoginEndpoint = "$baseUrlL/google-login";
+  // Olvido de contraseña
+  static const String forgotPasswordEndpoint = "$baseUrl/enviar-codigo-verificacion";
+  static const String verifyCodeEndpoint = "$baseUrl/verificar-codigo";
+  // Resetear contraseña sin login
+  static const String resetPasswordEndpoint = "$baseUrl/resetar-contrasenia";
 
 }
