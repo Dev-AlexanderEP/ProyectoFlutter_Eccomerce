c0045855cd4cd393cc03cb74e02a72f2a2a2726a
Dev-AlexanderEP <dev.alexander.estrada@gmail.com>
2025-09-27 17:29:17 -0500

    feat: Agregar para funciones para implementar el servicio ForgotPassImpl

--- lib/features/auth/controllers/AuthController.dart
+++ lib/features/auth/controllers/AuthController.dart
@@ -1,12 +1,16 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:proyecto_flutter/core/services/login/IAuthService.dart';
+import 'package:proyecto_flutter/core/services/login/forgotPassword/IForgotPass.dart';
 
 
 class AuthController extends ChangeNotifier {
   final IAuthService _authService;
+  final IForgotPass _forgotPass;
 
-  AuthController({required IAuthService authService})
-      : _authService = authService;
+  AuthController({required IAuthService authService, required IForgotPass forgotPass})
+      : _authService = authService, _forgotPass = forgotPass;
 
   String? _token;
   String? get token => _token;
@@ -20,4 +24,46 @@ class AuthController extends ChangeNotifier {
     }
     return false;
   }
+
+  Future<bool> sendForgotPasswordCode(String email) async {
+    // Generar código aleatorio de 5 dígitos
+    final random = Random();
+    final code = (10000 + random.nextInt(90000)).toString();
+    // Esto asegura que el número siempre tenga 5 dígitos (10000–99999)
+
+    // Llamar al servicio remoto
+    return await _forgotPass.sendVerificationCode(
+      email: email,
+      code: code,
+    );
+  }
+  Future<bool> verifyForgotPasswordCode({
+    required String email,
+    required String inputCode,
+  }) async {
+    try {
+      final ok = await _forgotPass.verifyCode(
+        email: email,
+        code: inputCode,
+      );
+
+      return ok;
+    } catch (e) {
+      // Aquí podrías hacer logging o manejar errores específicos
+      return false;
+    }
+  }
+  Future<bool> resetPassword({
+    required String newPassword,
+    String? email,
+    String? code,
+  }) async {
+    final success = await _authService.resetPassword(
+      newPassword: newPassword,
+      email: email,
+      code: code,
+    );
+    return success;
+  }
+
 }
