2b7c587130065a8092c4c87c9e9144a44c20c104
Dev-AlexanderEP <dev.alexander.estrada@gmail.com>
2025-10-12 23:19:17 -0500

    update: import actualizado app_routes.dart

--- lib/features/auth/pages/RegisterPage.dart
+++ lib/features/auth/pages/RegisterPage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../../../core/routes/app_routes.dart';
+import '../../../routes/app_routes.dart';
 import '../../../shared/widgets/AppButton.dart';
 import '../../../shared/widgets/AppTextField.dart';
 import '../../../shared/widgets/GoogleButton.dart';
--- lib/features/auth/pages/VerificationPage.dart
+++ lib/features/auth/pages/VerificationPage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../../../core/routes/app_routes.dart';
+import '../../../routes/app_routes.dart';
 import '../../../shared/widgets/AppButton.dart';
 import '../../../shared/widgets/TopBar.dart';
 import '../controllers/AuthController.dart';
