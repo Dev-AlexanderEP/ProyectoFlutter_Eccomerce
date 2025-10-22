ec9fe9ff1c48ff10939ca62214e753220a908e00
Dev-AlexanderEP <dev.alexander.estrada@gmail.com>
2025-10-21 23:14:59 -0500

    feat: update IRegisterService to use ApiResponse for registration response

--- lib/core/services/register/IRegisterService.dart
+++ lib/core/services/register/IRegisterService.dart
@@ -1,6 +1,6 @@
 import '../../../data/models/RegisterModel.dart';
+import '../../constants/ApiResponse.dart';
 
 abstract class IRegisterService {
-  Future<RegisterResponseModel?> register(String username, String email, String password);
-  Future<bool> verifyRegistrationCode(String email, String code);
+  Future<ApiResponse<RegisterResponseModel>?> register(String username, String email, String password);
 }
