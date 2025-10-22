1946ce805f8c232051a4a8c4169c4267ff000534
Dev-AlexanderEP <dev.alexander.estrada@gmail.com>
2025-10-21 23:10:59 -0500

    feat: update register method to return ApiResponse and refactor response handling

--- lib/core/services/register/RegisterServiceImpl.dart
+++ lib/core/services/register/RegisterServiceImpl.dart
@@ -1,5 +1,6 @@
 import '../../../data/RemoteRepositorie/register/RegisterRemoteRepository.dart';
 import '../../../data/models/RegisterModel.dart';
+import '../../constants/ApiResponse.dart';
 import 'IRegisterService.dart';
 
 class RegisterServiceImpl implements IRegisterService {
@@ -8,7 +9,7 @@ class RegisterServiceImpl implements IRegisterService {
   RegisterServiceImpl({required this.remoteDataSource});
 
   @override
-  Future<RegisterResponseModel?> register(String username, String email, String password) async {
+  Future<ApiResponse<RegisterResponseModel>?> register(String username, String email, String password) async {
     final result = await remoteDataSource.register(
       username: username,
       email: email,
@@ -16,21 +17,8 @@ class RegisterServiceImpl implements IRegisterService {
     );
 
     if (result != null) {
-      return RegisterResponseModel.fromJson(result);
+      return RegisterResponseModel.fromApiResponseJson(result);
     }
     return null;
-  }
-
-  @override
-  Future<bool> verifyRegistrationCode(String email, String code) async {
-    final result = await remoteDataSource.verifyRegistrationCode(
-      email: email,
-      code: code,
-    );
-
-    if (result != null) {
-      return result['success'] ?? false;
-    }
-    return false;
   }
 }
